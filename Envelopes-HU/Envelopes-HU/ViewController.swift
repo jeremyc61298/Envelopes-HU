a82e1f3ccbab597691f70499a1a79d06412a2c11
Jeremy Campbell <jcampbell2@harding.edu>
2019-04-12 15:28:26 -0500

    Changing the username and user email

---
+++ Envelopes-HU/Envelopes-HU/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Envelopes-HU
+//
+//  Created by Jeremy Campbell on 4/12/19.
+//  Copyright © 2019 Jeremy Campbell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
